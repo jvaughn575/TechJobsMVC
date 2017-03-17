33562caee1d6b334479c66fbade8c9a48be9df4f
James Vaughn <jvaughn575@gmail.com>
2017-03-17 10:08:19 -0500

    Completed the basic project goals

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,33 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        [Route("Search/Results")]        
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Dictionary<string, string>> results;
 
+            if (searchTerm is null)
+            {
+                searchTerm = "";
+            }
+            
+
+            if (searchType == "all")
+            {
+                results = JobData.FindByValue(searchTerm);
+            }
+            else
+            {
+                results = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+
+            
+            ViewBag.jobs = results;
+            ViewBag.columns = ListController.columnChoices;
+            return View("~/Views/Search/Index.cshtml");
+
+            //return Content("SearchType: " + searchType + " " + searchTerm);
+            //return Content("You Found Me!");
+        }
     }
 }
